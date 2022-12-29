9cdafd60a35469b3d808e80600108e01e50504b7
hyckis <hirakobanagi@gmail.com>
2022-12-29 23:07:47 +0800

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@
 
 public class Main {
 	public static void main(String[] args) {
-		Interface userInterface = new Interface();
-		userInterface.setSize(450, 700);
-		userInterface.setResizable(false);
-		userInterface.setVisible(true);
+		MemberQueries memberQueries = new MemberQueries();
+		memberQueries.setGroup();
 	}
 }
