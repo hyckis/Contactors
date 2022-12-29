c092d4ddceb8bff8660f631d458aaf80cf1016d4
hyckis <hirakobanagi@gmail.com>
2022-12-29 23:03:36 +0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// Author: YICHIN HO
+
+public class Main {
+	public static void main(String[] args) {
+		Interface userInterface = new Interface();
+		userInterface.setSize(450, 700);
+		userInterface.setResizable(false);
+		userInterface.setVisible(true);
+	}
+}
---
+++ src/MemberQueries.java
@@ -0,0 +1,134 @@
+// Author: YICHIN HO
+
+import java.sql.*;
+import java.util.*;
+
+public class MemberQueries {
+
+	private static final String URL = "jdbc:mysql://localhost:3306/member?autoReconnect=true&useSSL=false";
+	private static final String USERNAME = "java";
+	private static final String PASSWORD = "java";
+	private static final String DEFAULT_QUERY = "SELECT * FROM people";
+
+	Connection connection;
+	PreparedStatement selectAllMem;
+	PreparedStatement insertNewMem;
+	PreparedStatement deleteMem;
+	PreparedStatement updateMem;
+
+	static ResultSetTableModel tableModel;
+
+	public MemberQueries() {
+		try {
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			selectAllMem = connection.prepareStatement(DEFAULT_QUERY);
+			insertNewMem = connection.prepareStatement("INSERT INTO people " + "(name, type, phone) " + "VALUES(?, ?, ?)");
+			deleteMem = connection.prepareStatement("DELETE FROM people WHERE " + "(memberID = ?)");
+			updateMem = connection.prepareStatement("UPDATE people SET name = ?, type = ?, phone = ? WHERE "+"(memberID = ?)");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ResultSetTableModel getTableModel() {
+		try {
+			tableModel = new ResultSetTableModel(URL, USERNAME, PASSWORD, DEFAULT_QUERY);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			tableModel.disconnectFromDatabase();
+		}
+		return tableModel;
+	}
+	
+	public ResultSetTableModel getNameTable(String keywordOfName) {
+		String NAME_QUERY = "SELECT * FROM people WHERE name LIKE '%" + keywordOfName + "%'";
+		try {
+			tableModel = new ResultSetTableModel(URL, USERNAME, PASSWORD, NAME_QUERY);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			tableModel.disconnectFromDatabase();
+		}
+		return tableModel;
+	}
+	
+	public ResultSetTableModel getPhoneTable(String keywordOfPhone) {
+		String PHONE_QUERY = "SELECT * FROM people WHERE phone LIKE '%" + keywordOfPhone + "%'";
+		try {
+			tableModel = new ResultSetTableModel(URL, USERNAME, PASSWORD, PHONE_QUERY);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			tableModel.disconnectFromDatabase();
+		}
+		return tableModel;
+	}
+
+	public List<Member> getAllMember() {
+		ResultSet resultSet = null;
+		List<Member> results = new ArrayList<Member>();
+		try {
+			resultSet = selectAllMem.executeQuery();
+			while (resultSet.next()) {
+				results.add(new Member(
+					resultSet.getInt("memberID"),
+					resultSet.getString("name"),
+					resultSet.getString("type"),
+					resultSet.getString("phone")
+				));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				resultSet.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				close();
+			}
+		}
+		return results;
+	}
+
+	public void addMember(String name, String type, String phone) {
+		try {
+			insertNewMem.setString(1, name);
+			insertNewMem.setString(2, type);
+			insertNewMem.setString(3, phone);
+			int result = insertNewMem.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			close();
+		}
+	}
+
+	public void deleteMember(int memberID) {
+		try {
+			deleteMem.setInt(1, memberID);
+			int result = deleteMem.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			close();
+		}
+	}
+
+	public void updateMember(int memberID, String name, String type, String phone) {
+		try {
+			updateMem.setString(1, name);
+			updateMem.setString(2, type);
+			updateMem.setString(3, phone);
+			updateMem.setInt(4, memberID);
+			int result = updateMem.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			close();
+		}
+	}
+
+	public void close() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
